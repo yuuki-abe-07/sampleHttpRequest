563d424088df29d4bb871c1156a40a08868a05c7
abe-yuuk <abe-yuuk@k-opti.com>
2019-10-29 18:17:36 +0900

    fix: test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require('superagent')
 const API_BASEURL = 'http://localhost:7071/api/MyHttpTrigger'
 const main = async () => {
-  const i =0
+  const i =1
   const createData = async () => {
     const data = {name: 'abe'}
     return data
